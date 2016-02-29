af89386989c26f7cb0f0c882bca63309c6337848
tufedtm <tufedtm@gmail.com>
2016-02-29 08:55:57 +0300

    *news models posted default

--- news/models.py
+++ news/models.py
@@ -9,7 +9,7 @@ class New(models.Model):
     title = models.CharField('Заголовок', max_length=100, unique_for_date='posted')
     description = models.TextField('Краткое содержание')
     content = models.TextField('Полное содержание')
-    posted = models.DateTimeField('Опибликована', default=datetime.now())
+    posted = models.DateTimeField('Опибликована', default=datetime.now)
 
     def __unicode__(self):
         return self.title
