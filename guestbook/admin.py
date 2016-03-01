81b7a9d02e0f6f26bda2400f442382ff4a7bade1
tufedtm <tufedtm@gmail.com>
2016-03-01 11:55:15 +0300

    +admin for Guestbook

--- guestbook/admin.py
+++ guestbook/admin.py
@@ -1,4 +1,36 @@
 from django.contrib import admin
 from models import Guestbook
 
-admin.site.register(Guestbook)
+
+class GuestbookAdmin(admin.ModelAdmin):
+    actions_on_bottom = True
+    date_hierarchy = 'posted'
+    list_display = ['posted', 'user', 'content']
+    list_display_links = ['posted']
+    list_filter = ['user', 'posted']
+    preserve_filters = False
+    list_per_page = 5
+    list_max_show_all = 50
+    list_editable = ['content']
+    search_fields = ['user', 'content']
+    ordering = ['user', 'posted']
+
+    # fields = ['content']
+    # exclude = ['user']
+    # readonly_fields = ['posted']
+
+    save_as = False
+    save_on_top = True
+
+    fieldsets = [
+        ['dd', {
+            'classes': ['collapse'],
+            'fields': ['user'],
+        }],
+        ['ww', {
+            'fields': ['content']
+        }],
+    ]
+
+
+admin.site.register(Guestbook, GuestbookAdmin)
--- guestbook/models.py
+++ guestbook/models.py
@@ -12,6 +12,12 @@ class Guestbook(models.Model):
     def __unicode__(self):
         return self.user
 
+    def get_formatted_datetime(self):
+        return str(self.posted.day) + '.' + str(self.posted.month) + '.' + str(self.posted.year) + ' ' + str(
+            self.posted.hour) + ':' + str(self.posted.minute)
+
+    get_formatted_datetime.short_description = 'опубликовано'
+
     class Meta:
         ordering = ['-posted']
         verbose_name = 'запись гостевой книги'
